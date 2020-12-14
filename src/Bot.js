ad113c32568f2c7a456ba18f71b0163c3a8335fd
Luke-6723 <luke@ichigo.uk>
2020-12-14 05:12:08 +0000

    Bot Client

---
+++ src/Bot.js
@@ -0,0 +1,21 @@
+// Set max listeners to stop it throwing a "possible memory leak" warning
+process.setMaxListeners(250)
+// Dependencies
+const Base = require('eris-sharder').Base
+const { CommandHandler, EventHandler } = require('./modules')
+
+class Bot extends Base {
+  constructor() {
+    super()
+    // Load commands
+    this.client.commandHandler = new CommandHandler(this)
+    // Event Handler
+    this.client.eventHandler = new EventHandler(this)
+  }
+
+  launch() {
+    this.client.connect()
+  }
+}
+
+module.exports = Bot
