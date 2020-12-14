e86a0bf4c836b021754e48846c592f5beb95dfd3
Luke-6723 <luke@ichigo.uk>
2020-12-14 05:44:09 +0000

    Embed struct

---
+++ src/Util/Embed.js
@@ -0,0 +1,95 @@
+class Embed {
+  constructor () {
+    /**
+     * @returns {DiscordEmbed<Object>} A embed ready to be posted to discord.
+     */
+    this.title = ''
+    this.description = ''
+    this.type = 'rich'
+    this.color = null
+    this.url = ''
+    this.fields = []
+    this.author = {
+      name: '',
+      url: '',
+      icon_url: '',
+      proxy_icon_url: ''
+    }
+    this.footer = {
+      text: '',
+      icon_url: '',
+      proxy_icon_url: ''
+    }
+  }
+
+  /**
+   * @param {String} title The title of the embed
+   */
+  setTitle (title) {
+    this.title = title
+    return this
+  }
+
+  /**
+   * @param {String} description The description of the embed
+   */
+  setDescription (description) {
+    this.description = description
+    return this
+  }
+
+  /**
+   * @param {String} name The name of the embed field
+   * @param {String} value The content the embed field will contain
+   * @param {Boolean} inline Set whether the field will be inline or not
+   */
+  addField (name, value, inline = false) {
+    this.fields.push({ name: name, value: value, inline: inline })
+    return this
+  }
+
+  /**
+   * @param {String} text Footer text
+   * @param {String} iconURL Icon URL
+   * @param {String} proxyURL Proxy Icon URL
+   */
+  setFooter (text, iconURL, proxyURL) {
+    this.footer.text = text || 'undefined'
+    this.footer.icon_url = iconURL || null
+    this.footer.proxy_icon_url = proxyURL || null
+    return this
+  }
+
+  /**
+   * @param {String} name Author text
+   * @param {String} iconURL URL author will link to.
+   * @param {String} proxyURL Icon URL
+   * @param {String} iconProxyURL Proxy Icon URL
+   */
+  setAuthor (name, url, iconURL, iconProxyURL) {
+    this.author.name = name || 'undefined'
+    this.author.url = url || null
+    this.author.icon_url = iconURL || null
+    this.author.proxy_icon_url = iconProxyURL || null
+    return this
+  }
+
+  /**
+   * @param {Number | String} color Set the color of the embed
+   */
+  setColor (color) {
+    this.color = color
+    return this
+  }
+
+  /**
+   * @returns {Object} Outputs class values in JSON
+   */
+  toJSON () {
+    return {
+      ...this
+    }
+  }
+}
+
+module.exports = Embed
