6952dcc76896ef092e774bd818fcc5d7f419ad62
Luke-6723 <luke@ichigo.uk>
2020-12-14 05:42:28 +0000

    Add util to bot

--- src/Bot.js
+++ src/Bot.js
@@ -3,10 +3,13 @@ process.setMaxListeners(250)
 // Dependencies
 const Base = require('eris-sharder').Base
 const { CommandHandler, EventHandler } = require('./modules')
+const Util = require('./Util')
 
 class Bot extends Base {
   constructor(base) {
     super(base)
+    // Load Util
+    this.client.Util = Util
     // Load commands
     this.client.commandHandler = new CommandHandler(this)
     // Event Handler
