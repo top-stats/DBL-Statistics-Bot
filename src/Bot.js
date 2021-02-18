a74bb323268a6335ce7c7861fa5ce1b0e4737bb2
Luke-6723 <luke@ichigo.uk>
2021-02-18 03:51:53 +0000

    refactor

--- src/Bot.js
+++ src/Bot.js
@@ -6,10 +6,10 @@ const Base = require('eris-sharder').Base
 const { CommandHandler, EventHandler } = require('./modules')
 const Util = require('./Util')
 const config = require('../config.json')
-const express = require('express');
+const express = require('express')
 
 class Bot extends Base {
-  constructor(base) {
+  constructor (base) {
     super(base)
     // Set bot embed color
     this.client.color = 7506394
@@ -24,60 +24,57 @@ class Bot extends Base {
 
     // ==== BOT WEB API ====
 
-    const app = express();
+    const app = express()
 
     app.listen(config.PORT, config.HOST, () => {
-      console.log(`Listening on ${config.HOST}:${config.PORT}`);
-    });
+      console.log(`Listening on ${config.HOST}:${config.PORT}`)
+    })
 
     app.use(function (req, res, next) {
-      const auth = req.headers.authorization;
+      const auth = req.headers.authorization
 
-      if (auth === config.tokens.DBLStatistics) req.master = true;
+      if (auth === config.tokens.DBLStatistics) req.master = true
 
-      if (!req.master) return res.status(401).json({ code: 401, message: 'You are unauthorized to use this endpoint' });
+      if (!req.master) return res.status(401).json({ code: 401, message: 'You are unauthorized to use this endpoint' })
 
       next()
-    });
+    })
 
     const roles = {
-      'developer': '265125253443878912',
-      'auctions': '556148669242408971',
-      'moderator': '304313580025544704'
-    };
+      developer: '265125253443878912',
+      auctions: '556148669242408971',
+      moderator: '304313580025544704'
+    }
 
     app.get('/', (req, res) => {
       res.status(200).json({ message: 'Hello uwu' })
-    });
+    })
 
     app.get('/api/access/:role/:id', async (req, res) => {
-      const role = req.params.role;
-      const id = req.params.id;
+      const role = req.params.role
+      const id = req.params.id
 
-      if (!(role && id)) return res.status(400).json({ code: 400, message: 'No role or id provided with params' });
+      if (!(role && id)) return res.status(400).json({ code: 400, message: 'No role or id provided with params' })
 
-      const guild = this.client.guilds.get('264445053596991498');
+      const guild = this.client.guilds.get('264445053596991498')
 
-      if (!guild) return res.status(404).json({ code: 404, message: 'Guild not found' });
+      if (!guild) return res.status(404).json({ code: 404, message: 'Guild not found' })
 
-      const foundRole = guild.roles.get(roles[role]);
+      const foundRole = guild.roles.get(roles[role])
 
-      if (!foundRole) return res.status(404).json({ code: 404, message: 'Role not found' });
+      if (!foundRole) return res.status(404).json({ code: 404, message: 'Role not found' })
 
-      const foundMember = guild.members.get(id) || (await guild.fetchMembers({ userIDs: [ id ] }))[0]
-      if(!guild.members.get(id))
+      const foundMember = guild.members.get(id) || (await guild.fetchMembers({ userIDs: [id] }))[0]
+      if (!guild.members.get(id)) { if (!foundMember) return res.status(404).json({ code: 404, message: 'Member not found' })}
 
-      if (!foundMember) return res.status(404).json({ code: 404, message: 'Member not found' });
+      const hasRole = foundMember.roles.includes(foundRole.id)
 
-      const hasRole = foundMember.roles.includes(foundRole.id);
-
-      if (hasRole) return res.status(200).json({ has_access: true });
-      else res.status(401).json({ has_access: false });
-    });
-
+      if (hasRole) return res.status(200).json({ has_access: true })
+      else res.status(401).json({ has_access: false })
+    })
   }
 
-  launch() {
+  launch () {
     this.client.connect()
   }
 }
