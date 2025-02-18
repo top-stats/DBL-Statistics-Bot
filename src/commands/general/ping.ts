4f559b1a8fe8fc33d7bf5f496aa0bde41ded6b5e
Luke Stoodley <luke@ichigo.uk>
2025-02-18 09:24:35 +0000

    Push example for topStatsAPI

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -1,4 +1,5 @@
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import topStatsAPI from "../../util/topstats";
 
 const pingCommandData = new SlashCommandBuilder()
   .setName('ping')
@@ -7,6 +8,7 @@ const pingCommandData = new SlashCommandBuilder()
 export default {
   data: pingCommandData,
   execute: async (interaction: CommandInteraction) => {
-    interaction.reply('Pong!')
+    const bot = await topStatsAPI.getBot('583807014896140293')
+    interaction.reply('Pong! We have ' + bot.monthly_votes + ' votes this month')
   }
 };
--- src/util/topstats.ts
+++ src/util/topstats.ts
@@ -1,7 +1,7 @@
 import { Client } from '@topstats/sdk/dist'
 
 const topStatsAPI = new Client({
-  token: process.env.TOP_GG_TOKEN || ""
+  token: process.env.TOPSTATS_API_TOKEN || ""
 })
 
 export default topStatsAPI;
