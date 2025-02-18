7a888be67b7b30ac66276ac93805783c6be31c46
Luke Stoodley <luke@ichigo.uk>
2025-02-18 08:56:51 +0000

    tabs to spaces

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -6,25 +6,25 @@ export default async function InteractionCreate(interaction: Interaction) {
 
   const command = commandHandler.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ 
-        content: 'There was an error while executing this command!', 
-        flags: MessageFlags.Ephemeral 
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: 'There was an error while executing this command!',
+        flags: MessageFlags.Ephemeral
       });
-		} else {
-			await interaction.reply({ 
-        content: 'There was an error while executing this command!', 
-        flags: MessageFlags.Ephemeral 
+    } else {
+      await interaction.reply({
+        content: 'There was an error while executing this command!',
+        flags: MessageFlags.Ephemeral
       });
-		}
-	}
+    }
+  }
 }
